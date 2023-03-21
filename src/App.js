ee82d8e566729b3b776c6d60060bed0d43997531
NILOY ROY <121684663+xanon-oc@users.noreply.github.com>
2023-03-21 20:15:55 +0600

    Task Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   return (
     <div className="App">
       <Counter></Counter>
+      <ExternalUsers></ExternalUsers>
+    </div>
+  );
+}
+
+function ExternalUsers() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+  return (
+    <div>
+      <h1>External Users : </h1>
+      <p>{users.length}</p>
+      {users.map((user) => (
+        <User name={user.name} email={user.email}></User>
+      ))}
+    </div>
+  );
+}
+
+function User(props) {
+  return (
+    <div style={{ border: "2px solid green", margin: "20px" }}>
+      <h3>Name : {props.name}</h3>
+      <p>Email : {props.email}</p>
     </div>
   );
 }
 
 function Counter() {
   const [count, setCount] = useState(0);
-  const increaseCount = () => {
-    const newCount = count + 1;
-    setCount(newCount);
-  };
-  const decreaseCount = () => {
-    const oldCount = count - 1;
-    setCount(oldCount);
-  };
+  const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1);
+
   return (
     <div>
       <h1>COUNT : {count} </h1>
